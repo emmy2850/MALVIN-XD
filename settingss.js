c2fca42c94d4041a15718c996398edef92de511e
emmy2850 <ojoe3119@gmail.com>
2025-08-05 20:38:51 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~0zUVnYqT#f8H9pJnIYEirHXZwOwySsyQwz1XzjAKOtaXEIzxYnZM",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2348085898997", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
