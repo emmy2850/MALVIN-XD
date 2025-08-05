05f7c44d9357330d71c84acdf8120fe252a562b0
emmy2850 <ojoe3119@gmail.com>
2025-08-05 20:55:19 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~0zUVnYqT#f8H9pJnIYEirHXZwOwySsyQwz1XzjAKOtaXEIzxYnZM",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
